904e60afc294f3976a99fa33fece12e6978c66ec
Josh_DESKTOP <Josh@DESKTOP-32TFCUU>
2021-03-04 14:19:32 +0100

    made a more "complex" case

--- src/test/java/books/BookShopTest.java
+++ src/test/java/books/BookShopTest.java
@@ -12,8 +12,8 @@ public class BookShopTest {
 
     @Test
     public void costTest(){
-        int[] books= {1,1,1,1,1};
-        double expectedCost=5*8*0.35;
+        int[] books= {1,2,1,3,1};
+        double expectedCost=5*8*0.35 + 3*8;
         double cost= new BookShop("TestShop").cost(books);
         assertTrue(cost==expectedCost);
     }
