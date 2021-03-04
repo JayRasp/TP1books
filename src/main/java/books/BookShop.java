39554603e3b0c80938ba9e66250ae02819ed6e56
Josh_DESKTOP <Josh@DESKTOP-32TFCUU>
2021-03-04 14:20:17 +0100

    formated all code

--- src/main/java/books/BookShop.java
+++ src/main/java/books/BookShop.java
@@ -6,44 +6,47 @@ public class BookShop {
 
     /**
      * Constructor of the class Book shop
+     *
      * @param name name of the book shop
      */
-    public BookShop(String name){
+    public BookShop(String name) {
         this.name = name;
     }
 
     /**
      * method to compute the cost of a basket
-     * @param books array corresponding to the number of each harry potter book the client desire to buy (books.length should return 5)
+     *
+     * @param books array corresponding to the number of each harry potter book
+     * the client desire to buy (books.length should return 5)
      * @return the cost in euro with the discount
      */
-    public double cost(int[] books){
+    public double cost(int[] books) {
         //TODO complete
-        int diffBooks=0;
-        int totalBooks=0;
-        
-        for(int i : books){
-            if(i>0){
+        int diffBooks = 0;
+        int totalBooks = 0;
+
+        for (int i : books) {
+            if (i > 0) {
                 diffBooks++;
             }
-            totalBooks+=i;
+            totalBooks += i;
         }
-        double discount=0;
-        switch(diffBooks){
+        double discount = 0;
+        switch (diffBooks) {
             case 2:
-                discount=0.07;
+                discount = 0.07;
                 break;
             case 3:
-                discount=0.14;
+                discount = 0.14;
                 break;
             case 4:
-                discount=0.28;
+                discount = 0.28;
                 break;
             case 5:
-                discount=0.35;
+                discount = 0.35;
                 break;
         }
-        
-        return (totalBooks-diffBooks)*8+ discount*diffBooks*8;
+
+        return (totalBooks - diffBooks) * 8 + discount * diffBooks * 8;
     }
 }
--- src/test/java/books/BookShopTest.java
+++ src/test/java/books/BookShopTest.java
@@ -7,14 +7,13 @@ import books.BookShop;
 /**
  * TODO : Add your import here
  */
-
 public class BookShopTest {
 
     @Test
-    public void costTest(){
-        int[] books= {1,2,1,3,1};
-        double expectedCost=5*8*0.35 + 3*8;
-        double cost= new BookShop("TestShop").cost(books);
-        assertTrue(cost==expectedCost);
+    public void costTest() {
+        int[] books = {1, 2, 1, 3, 1};
+        double expectedCost = 5 * 8 * 0.35 + 3 * 8;
+        double cost = new BookShop("TestShop").cost(books);
+        assertTrue(cost == expectedCost);
     }
 }
